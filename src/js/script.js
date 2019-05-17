6a28517aedd1115f76705e829d0422092ffbf883
ArturHatoka <hameleon2011007@mail.ru>
2019-05-17 13:18:44 +0300

    add position

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,33 @@ function slider(event) {
     $x = parseFloat($f.x.value);
     this.querySelectorAll('.slider__box').forEach(slider__box =>{
         let speed = slider__box.getAttribute('data-speed');
-        slider__box.style.transform = `translateX(${-(($x-75)*speed)}px)`;
-        console.log(slider__box.style.transition = `${0.025*speed}s`);
+        slider__box.style.transform = `translateX(${-($x-250)*speed}px)`;
+        slider__box.style.transition = `${0.025*speed}s`;
     });
 }
 
 document.addEventListener('mousemove', slider);
 
 $(document).ready(function () {
-    $('#video').hover(
+    this.querySelectorAll('.slider__box').forEach(slider__box =>{
+        let position = slider__box.getAttribute('data-position');
+        slider__box.style.left = `${position*10}%`;
+    });
+});
+
+$(document).ready(function () {
+    $('.video').hover(
         function () {
-            $(this).children('video').fadeIn(200).css("display", "flex").css("top", "-10px").css("width", "100%");
+            $(this).children('video').css("display", "flex").css("top", "-15px").css("width", "120%");
+            $(this).addClass('active');
+            $(this).find('.slider__box__body-desc').addClass('active');
+            $(this).find('.slider__box-details').addClass('active');
         },
         function () {
-            $(this).children('video').css("top", "-200px").css("width", "80%").fadeOut(500);
+            $(this).children('video').css("top", "-250px").css("width", "100%");
+            $(this).removeClass('active');
+            $(this).find('.slider__box__body-desc').removeClass('active');
+            $(this).find('.slider__box-details').removeClass('active');
         }
     );
 });
